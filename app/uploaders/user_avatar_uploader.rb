a3e22e34685cf5927fb169ad226330a90a929743
Anthonyhelka <helkaanthony1997@gmail.com>
2019-11-27 12:50:48 -0500

    Added default user avatar.

--- app/uploaders/user_avatar_uploader.rb
+++ app/uploaders/user_avatar_uploader.rb
@@ -4,4 +4,8 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def default_url(*args)
+    ActionController::Base.helpers.image_url("default.png")
+  end
 end
